42d8f7cbc404482f607554d6a3a2c4f276941f1d
Abdelfattah1990 <abdew879@gmail.com>
2022-09-08 04:57:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+/** 
+ *
+ *  * main - entry point 
+ *
+ *   * 
+ *
+ *    * Return: Displays a message to the terminal. 
+ *
+ *     */ 
+
+int main(void) 
+
+{ 
+
+	 puts("\"Programming is like building a multilingual puzzle"); 
+
+	  return (0); 
+
+}
+
+
---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,22 @@
+
+/** 
+ *
+ *  * main - entry point  
+ *
+ *   * 
+ *
+ *    * Return: Displays a message to the terminal. 
+ *
+ *     */ 
+
+int main(void) 
+
+{ 
+
+	 puts("\"Programming is like building a multilingual puzzle"); 
+
+	  return (0); 
+
+}
+
+
