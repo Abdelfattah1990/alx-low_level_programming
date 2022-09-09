147757e71a8cff82b89e59abf12db63d51ec20c4
Abdelfattah1990 <abdew879@gmail.com>
2022-09-09 04:03:50 -0700

    task4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entery is here
+ *
+ *   * Description: small and capital alphabets
+ *
+ *    * Return: always 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int a;
+
+			int b;
+
+
+
+				for (a = 'a'; a <= 'z'; a++)
+
+						{
+
+									putchar(a);
+
+										}
+
+					for (b = 'A'; b <= 'Z'; b++)
+
+							{
+
+										putchar(b);
+
+											}
+
+						putchar(10);
+
+							return (0);
+
+}
