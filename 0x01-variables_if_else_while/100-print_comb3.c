a8f8fb9f97cb0ecc944a3632211b5e05dd81fbbc
Abdelfattah1990 <abdew879@gmail.com>
2022-09-09 04:25:46 -0700

    task11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *
+ *  * main - entery
+ *
+ *   * description: 00-99
+ *
+ *    * Return: always 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int a;
+
+			int b;
+
+
+
+				for (a = 48; a <= 57; a++)
+
+						{
+
+									for (b = 49; b <= 57; b++)
+
+												{
+
+																if (b > a)
+
+																				{
+
+																								putchar(a);
+
+																											putchar(b);
+
+																														if (a != 56 || b != 57)
+
+																																		{
+
+																																						putchar(44);
+
+																																									putchar(32);
+
+																																												}
+
+																																	}
+
+																		}
+
+										}
+
+					putchar(10);
+
+						return (0);
+
+}
