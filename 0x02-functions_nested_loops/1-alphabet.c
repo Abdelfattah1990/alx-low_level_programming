8132d5ab0dee5821c45dfbbb2939eb808c917ddd
Abdelfattah1990 <abdew879@gmail.com>
2022-09-13 04:17:47 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  *main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+void print_alphabet(void)
+
+{
+
+		char alpha = 'a';
+
+
+
+			while (alpha <= 'z')
+
+					{
+
+								_putchar(alpha);
+
+										alpha++;
+
+											}
+
+
+
+				_putchar('\n');
+
+}
+
+
