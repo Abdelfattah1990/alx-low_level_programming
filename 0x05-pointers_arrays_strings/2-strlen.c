1c98bccfa30a45e73ba2a91aac59e344dc93ce7c
Abdelfattah1990 <abdew879@gmail.com>
2022-09-19 03:40:30 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _strlen - returns the length
+ *
+ *   * @s: String whose length we want to determine
+ *
+ *    *
+ *
+ *     * Return: length of String
+ *
+ *      */
+
+int _strlen(char *s)
+
+{
+
+		int length;
+
+			int i;
+
+
+
+				length = 0;
+
+					i = 0;
+
+						while (s[i] != '\0')
+
+								{
+
+											++length;
+
+													i++;
+
+														}
+
+
+
+							return (length);
+
+}
+
