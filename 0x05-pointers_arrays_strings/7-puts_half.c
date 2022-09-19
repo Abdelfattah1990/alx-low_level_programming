dd88d2c42fb7a0dc311c88c555358119957c00d1
Abdelfattah1990 <abdew879@gmail.com>
2022-09-19 04:14:47 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,49 @@
+nclude <string.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * puts_half - print the last half of string
+ *
+ *   * @s: pointer to the string variable s
+ *
+ *    *
+ *
+ *     * Return: Null
+ *
+ *      */
+
+void puts_half(char *s)
+
+{
+
+		int len = strlen(s), i;
+
+
+
+			if (len % 2 == 0)
+
+					{
+
+								for (i = len / 2; i < len; i++)
+
+												_putchar(s[i]);
+
+									}
+
+				else
+
+						{
+
+									for (i = len / 2 + 1; i < len; i++)
+
+													_putchar(s[i]);
+
+										}
+
+					_putchar('\n');
+
+}
