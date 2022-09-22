a69f92ba5b044ef11f2f73df186164409644767b
Abdelfattah1990 <abdew879@gmail.com>
2022-09-22 08:33:29 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,32 +1,43 @@
 #include "main.h"
 
+
+
 /**
- * _strncat - Concatenates one string to another
+ * _strncat - Concatenates strings
  * @dest: Destination string
  * @src: String to be concatenated
  * @n: number of bytes to be copied
- * Return: The destination string
+ * Return: destination string
  */
 
 char *_strncat(char *dest, char *src, int n)
 
 {
 
-	int i;
-	int destlen = 0;
-	int counter = 0;
+		int i;
+		int destlen = 0;
+		int counter = 0;
 
-	while (dest[counter] != '\0')
-	{
+
+
+		while (dest[counter] != '\0')
+
+{
 
 		destlen++;
-		counter++;
 
-	}
+counter++;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+}
+
+		for (i = 0; i < n && src[i] != '\0'; i++)
+
 		dest[destlen + i] = src[i];
+
 		dest[destlen + i] = '\0';
 
-	return (dest);
+
+
+		return (dest);
+
 }
