53c1034280caa4bbff54fa19ca3ccca1ef2e5ac2
Abdelfattah1990 <abdew879@gmail.com>
2022-09-26 04:31:27 -0700

    Main

---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: main.c
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char *s = "hello, world";
+	char *f = "oleh";
+	unsigned int n;
+
+		n = _strspn(s, f);
+		printf("%u\n", n);
+
+	return (0);
+}
