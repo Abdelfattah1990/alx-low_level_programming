6cc7aababf78e16f94936327ee67a32574037e68
Abdelfattah1990 <abdew879@gmail.com>
2022-12-12 02:49:31 -0800

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,6 @@
 #include<time.h>
 #include<stdio.h>
 /* what headers */ 
-
- 
-
 /** 
  *
  *  * main - Entery is here 
@@ -19,40 +16,38 @@ int main(void)
 
 { 
 
-	 int n; 
+int n; 
 
-	  
+srand(time(0)); 
 
-	  srand(time(0)); 
+n = rand() - RAND_MAX / 2; 
 
-	   n = rand() - RAND_MAX / 2; 
+/* your code goes there */ 
 
-	    /* your code goes there */ 
+if (n > 0) 
 
-	    if (n > 0) 
+{ 
 
-		     { 
+printf("%d is positive\n", n); 
 
-			       printf("%d is positive\n", n); 
+} 
 
-			        } 
+else if (n == 0) 
 
-	     else if (n == 0) 
+{ 
 
-		      { 
+printf("%d is zero\n", n); 
 
-			        printf("%d is zero\n", n); 
+} 
 
-				 } 
+else 
 
-	      else 
+{ 
 
-		       { 
+printf("%d is negative\n", n); 
 
-			         printf("%d is negative\n", n); 
+} 
 
-				  } 
-
-	       return (0); 
+return (0); 
 
 }
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,23 +3,19 @@
 
 
 /**
- *
  *  * reset_to_98 - resets the value of a variable
  *
  *   * @n: Pointer to the variable
  *
  *    *
  *
- *     * Description: This function takes a pointer to an integer variable as an
- *
- *      * argument and uses that pointer to change the value of that variable to 98
+ *   * Description: This function takes a pointer
+ *   * argument and uses that pointer to change 
  *
  *       * from inside the function.
  *
  *        * Return: void
- *
- *         */
-
+     */
 void reset_to_98(int *n)
 
 {
